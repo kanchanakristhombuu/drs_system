0ed1575789720ebe0dafc6d3512cf837623fa874
kanchanakristhombu97 <12260590@cqumail.com>
2025-06-15 21:59:03 +1000

    Added JUnit Testing for Reports

---
+++ src/test/java/com/mycompany/disastermanagementsystem/dao/ReportTest.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.disastermanagementsystem.dao;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.mycompany.disastermanagementsystem.models.Report;
+
+/**
+ *
+ * @author Kanch
+ */
+
+public class ReportTest {
+    @Test
+    public void testGettersAndSetters() {
+        Report report = new Report("Flood", 2, "0712345678", "Sunnybank");
+        report.setEmergencyType("Fire");
+        report.setSeverity(3);
+        report.setContactNumber("0777777777");
+        report.setAddress("Runcorn");
+
+        assertEquals("Fire", report.getEmergencyType());
+        assertEquals(3, report.getSeverity());
+        assertEquals("0777777777", report.getContactNumber());
+        assertEquals("Runcorn", report.getAddress());
+        assertNotNull(report.getReportID());
+        assertNotNull(report.getDate());
+    }
+}
+
